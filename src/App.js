6efce0ab5ed5b700468dec93bd940690578958c7
Alex Arriaga <alexzander.arriaga@base22.com>
2019-02-26 19:45:46 -0600

    Agregar Material UI interface

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Layout from './components/layout';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './components/home';
+import Blog from './components/blog';
+import Editor from './components/editor';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <BrowserRouter>
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/blog" component={Blog}/>
+              <Route exact path="/blog/posts/new" component={Editor}/>
+              {/*<Route exact path="/:slug" component={Home}/>*/}
+            </Switch>
+          </Layout>
+        </BrowserRouter>
+
     );
   }
 }
---
+++ src/components/layout/index.js
@@ -0,0 +1,162 @@
+import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import {
+  AppBar,
+  CssBaseline,
+  Divider,
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import ListIcon from '@material-ui/icons/List';
+import CreateIcon from '@material-ui/icons/Create';
+
+import {NavLink} from 'react-router-dom';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative',
+    },
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+class Layout extends React.Component {
+  state = {
+    mobileOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState(state => ({mobileOpen: !state.mobileOpen}));
+  };
+
+  render() {
+    const {children, classes, theme} = this.props;
+
+    const drawer = (
+        <div>
+          <Hidden smDown>
+            <div className={classes.toolbar}/>
+          </Hidden>
+          <Divider/>
+          <List component="nav">
+            <ListItem component={NavLink} to="/" button>
+              <ListItemIcon>
+                <HomeIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Home"/>
+            </ListItem>
+            <ListItem component={NavLink} to="/blog" button>
+              <ListItemIcon>
+                <ListIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Blog"/>
+            </ListItem>
+            <Divider/>
+            <ListItem component={NavLink} to="/blog/posts/new" button>
+              <ListItemIcon>
+                <CreateIcon/>
+              </ListItemIcon>
+              <ListItemText primary="New Post"/>
+            </ListItem>
+          </List>
+        </div>
+    );
+
+    return (
+        <Fragment>
+          <CssBaseline/>
+          <div className={classes.root}>
+            <AppBar className={classes.appBar} position="absolute">
+              <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="Open drawer"
+                    onClick={this.handleDrawerToggle}
+                    className={classes.navIconHide}
+                >
+                  <MenuIcon/>
+                </IconButton>
+                <Typography variant="h6" color="inherit" noWrap>
+                  Carbon LDP / React Blog Engine
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Hidden mdUp>
+              <Drawer
+                  variant="temporary"
+                  anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                  open={this.state.mobileOpen}
+                  onClose={this.handleDrawerToggle}
+                  classes={{
+                    paper: classes.drawerPaper,
+                  }}
+                  ModalProps={{
+                    keepMounted: true, // Better open performance on mobile.
+                  }}
+              >
+                {drawer}
+              </Drawer>
+            </Hidden>
+            <Hidden smDown implementation="css">
+              <Drawer
+                  variant="permanent"
+                  open
+                  classes={{
+                    paper: classes.drawerPaper,
+                  }}
+              >
+                {drawer}
+              </Drawer>
+            </Hidden>
+            <main className={classes.content}>
+              <div className={classes.toolbar}/>
+              {children}
+            </main>
+          </div>
+        </Fragment>
+    );
+  }
+}
+
+Layout.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, {withTheme: true})(Layout);
